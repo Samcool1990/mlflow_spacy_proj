03c5dd70b237e98ace1b7268dbb1f81939c9db67
Suman Pathak <suman.pathak@smartims.com>
2024-07-15 15:52:54 +0530

    model 5 added

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from spacy.pipeline import EntityRuler
 import mlflow
 
 
-import dagshub
-dagshub.init(repo_owner='Samcool1990', repo_name='mlflow_spacy_proj', mlflow=True, root='./Artifacts')
+# import dagshub
+# dagshub.init(repo_owner='Samcool1990', repo_name='mlflow_spacy_proj', mlflow=True, root='./Artifacts')
 
 # Download and load models
 stanza.download("en")  # Download Stanza model for English
@@ -61,12 +61,12 @@ if "medra_ruler" not in nlp_combined.pipe_names:
 # Start an MLflow run with experiment management
 if __name__ == "__main__":
     # Create a new mlflow experiment
-    experiment_name = "spacy_stanza_combined_model_experiment_4"
+    experiment_name = "spacy_stanza_combined_model_experiment_5"
     try:
         experiment_id = mlflow.create_experiment(
             name=experiment_name,
             artifact_location="./mlruns",
-            tags={"env": "dev", "version": "1.3.0"},
+            tags={"env": "dev", "version": "1.1.0"},
         )
     except mlflow.exceptions.MlflowException:
         experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
@@ -76,16 +76,16 @@ if __name__ == "__main__":
 
         # Log parameters, metrics, and model
         mlflow.log_param("iterations", 10)
-        mlflow.spacy.log_model(spacy_model=nlp_combined, artifact_path="spacy_combined_model_experiment_3")
+        mlflow.spacy.log_model(spacy_model=nlp_combined, artifact_path="spacy_combined_model_experiment_5")
 
         print(f"Run ID: {run_id}")
 
     # Register the Model
-    model_name = "spacy_combined_ner_model_stanza_test_experiment_model_4"
+    model_name = "spacy_combined_ner_model_stanza_test_experiment_model_5"
 
     try:
         result = mlflow.register_model(
-            model_uri=f"runs:/{run_id}/spacy_combined_model_experiment_4",
+            model_uri=f"runs:/{run_id}/spacy_combined_model_experiment_5",
             name=model_name,
         )
         print(f"Model registered as {result.name} with version {result.version}")
